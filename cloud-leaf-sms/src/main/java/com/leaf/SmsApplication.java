9fb57800fc28f92b635fb8fee7d75e0daf1374f3
liuk <yezi12237@163.com>
2021-04-29 11:05:50 +0800

    Initial commit

---
+++ cloud-leaf-sms/src/main/java/com/leaf/SmsApplication.java
@@ -0,0 +1,10 @@
+package com.leaf;
+
+import org.springframework.boot.SpringApplication;
+
+public class SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+}
