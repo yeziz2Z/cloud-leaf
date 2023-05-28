64ad626ccc16b07fc73b2912af2e558d2c02d632
yezi <yezi12237@163.com>
2023-05-28 21:39:20 +0800

    1.升级jdk17

---
+++ cloud-leaf-oauth2/src/main/java/com/leaf/oauth2/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.leaf.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author liuk
+ */
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain() {
+        return null;
+    }
+}
